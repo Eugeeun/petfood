8ac4c0a465c5d21134a807b055aea41691db6bb1
JongWon <jack603@naver.com>
2023-05-01 14:31:11 +0900

    init

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`${PORT} listening!`));
