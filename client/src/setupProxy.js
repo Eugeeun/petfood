85e2fa4a4c0b68bf9df8eb6324b9c12daec2be6c
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2023-05-08 17:06:01 +0900

    주석 추가

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/api', // api로 시작하는 주소가 오면
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://localhost:5000', // 이 주소를 목적지로 함!
       changeOrigin: true,
     })
   );
