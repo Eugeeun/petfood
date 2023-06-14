c54c35fe9b2c9547000acc440e9fc7938524fbbe
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2023-06-14 20:36:40 +0900

    메인페이지에 주석 추가

--- client/src/components/page/MainPage.js
+++ client/src/components/page/MainPage.js
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
+// 페이지 전체 스타일
 const Page = styled.div`
   max-width: 1000px;
   margin: 50px auto;
 `;
 
+// 컨텐츠1 스타일
 const Content1 = styled.div`
   padding: 20px;
   border: 1px solid #ccc;
@@ -15,6 +17,7 @@ const Content1 = styled.div`
   margin: 0 12px;
 `;
 
+// 입력 타이틀 스타일
 const InputTitle = styled.div`
   display: flex;
   justify-content: center;
@@ -24,6 +27,8 @@ const InputTitle = styled.div`
   margin-bottom: 20px;
   color: navy;
 `;
+
+// 입력 폼 그룹 스타일
 const FormGroup = styled.div`
   display: inline-flex;
   flex-direction: column;
@@ -53,7 +58,7 @@ const FormGroup = styled.div`
     }
   }
 
-  //모바일 환경 또는 가로폭이 1000px 이하로 줄어들면 밑에 css로 적용
+  // 모바일 환경 또는 가로폭이 1000px 이하로 줄어들면 적용되는 스타일
   @media screen and (max-width: 1000px) {
     display: flex;
     flex-direction: column;
@@ -80,11 +85,13 @@ const FormGroup = styled.div`
   }
 `;
 
+// 설명 스타일
 const Explain = styled.span`
   font-size: 13px;
   color: red;
 `;
 
+// 제출 버튼 스타일
 const SubmitButton = styled.button`
   height: 35px;
   width: 120px;
@@ -105,6 +112,7 @@ const SubmitButton = styled.button`
   }
 `;
 
+// 컨텐츠2 스타일
 const Content2 = styled.div`
   max-width: 1000px;
   border: 1px solid #ccc;
@@ -112,6 +120,7 @@ const Content2 = styled.div`
   margin: 20px 12px;
 `;
 
+// 배너 이미지 스타일
 const BannerImg = styled.img`
   width: 100%;
 `;
@@ -123,23 +132,28 @@ function MainPage() {
   const [notAllow, setNotAllow] = useState(true);
   const [result, setResult] = useState('');
 
-  const handlespeciesChange = (event) => {
+  // 종 변경 핸들러
+  const handleSpeciesChange = (event) => {
     setSpecies(event.target.value);
   };
 
+  // 품종명 변경 핸들러
   const handleBreedChange = (event) => {
     setBreed(event.target.value);
   };
 
+  // 음식명 변경 핸들러
   const handleFoodNameChange = (event) => {
     setFoodName(event.target.value);
   };
 
   useEffect(() => {
+    // 종과 음식명이 비어있으면 제출 버튼 비활성화
     setNotAllow(!(species.length > 0 && foodName.length > 0));
 
     const userId = localStorage.getItem('id');
     if (userId) {
+      // 서버에 사용자의 반려동물 정보 요청
       Axios.post('/api/petinfo', { id: userId }).then((response) => {
         if (response.data.success) {
           const petData = response.data.rows[0];
@@ -150,6 +164,7 @@ function MainPage() {
     }
   }, [species, foodName]);
 
+  // 폼 제출 핸들러
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -158,6 +173,7 @@ function MainPage() {
       foodName: foodName,
     };
 
+    // 서버에 음식 유해도 검색 요청
     Axios.post('/api/edibility', searchData).then((response) => {
       if (!response.data.success) {
         setResult('알 수 없는 음식 또는 존재하지 않는 품종');
@@ -180,7 +196,7 @@ function MainPage() {
               type="text"
               placeholder=" 개"
               value={species}
-              onChange={handlespeciesChange}
+              onChange={handleSpeciesChange}
             />
           </FormGroup>
           <FormGroup>
